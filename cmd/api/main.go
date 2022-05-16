e9180c34b94c6f9564f65134d9a0e0ab26d60e14
Hasyimi Bahrudin <hasyimi@ridebeam.com>
2022-05-17 01:26:55 +0800

    Shutdown gracefully via signal interrupt

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,13 +1,35 @@
 package main
 
 import (
+	"context"
+	"log"
+	"net/http"
 	"omshub/core-api/internal/api"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
-	server := api.NewServer(api.Config{
-		Port: "1927",
-	})
+	// TODO: read from file or env vars
+	config := api.DefaultConfig()
+	server := api.NewServer(config)
 
-	_ = server.Serve()
+	go func() {
+		if err := server.Serve(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("[error] serve: %s\n", err)
+		}
+	}()
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	<-ctx.Done()
+
+	stop()
+
+	log.Println("[info] shutting down")
+
+	if err := server.Shutdown(); err != nil {
+		log.Fatalf("[error] shutdown: %s\n", err)
+	}
 }
--- internal/api/config.go
+++ internal/api/config.go
@@ -1,5 +1,16 @@
 package api
 
+import "time"
+
 type Config struct {
-	Port string
+	Port             string
+	ShutdownDuration time.Duration
+}
+
+// TODO: use a library to manage sensible defaults via struct tags
+func DefaultConfig() Config {
+	return Config{
+		Port:             "1927",
+		ShutdownDuration: time.Second * 5,
+	}
 }
