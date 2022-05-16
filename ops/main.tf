36feb79b3b0dacae3799319baa997446da176603
James Liu <james@jamesliu.io>
2022-05-16 21:56:49 +1000

    ci: create DO app with Terraform

--- ops/main.tf
+++ ops/main.tf
@@ -1,7 +1,48 @@
 terraform {
+  cloud {
+    organization = "omshub"
+    workspaces {
+      name = "core-api"
+    }
+  }
+
   required_providers {
-    tfe = {
-      version = "~> 0.30.2"
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+variable "do_token" {}
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
+resource "digitalocean_app" "app-core-api" {
+  spec {
+    name   = "core-api"
+    region = "nyc1"
+
+    service {
+      name               = "core-api"
+      instance_count     = 1
+      instance_size_slug = "basic-xxs"
+
+      http_port = 1927
+
+      dockerfile_path = "Dockerfile"
+
+      routes {
+        path = "/"
+      }
+
+      github {
+        repo           = "omshub/core-api"
+        branch         = "ci/digitalocean-app-platform"
+        deploy_on_push = false
+      }
     }
   }
 }
--- ops/tfe.tf
+++ ops/tfe.tf
@@ -1,11 +1,11 @@
 # Resources for Terraform Cloud
 
-resource "tfe_organization" "org-omshub" {
-  name  = "omshub"
-  email = "terraform@omshub.org"
-}
+# resource "tfe_organization" "org-omshub" {
+#   name  = "omshub"
+#   email = "terraform@omshub.org"
+# }
 
-resource "tfe_workspace" "workspace-core-api" {
-  name         = "core-api"
-  organization = tfe_organization.org-omshub.name
-}
+# resource "tfe_workspace" "workspace-core-api" {
+#   name         = "core-api"
+#   organization = tfe_organization.org-omshub.name
+# }
