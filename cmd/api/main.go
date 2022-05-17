04861779b45ae27be16af0f4f6b6a0b63710f356
ashiquem <ashiquemahmood@outlook.com>
2022-05-17 21:06:43 +1000

    feat: added app config management

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"log"
 	"omshub/core-api/internal/api"
+
+	"github.com/ilyakaznacheev/cleanenv"
 )
 
 func main() {
-	server := api.NewServer(api.Config{
-		Port: "1927",
-	})
+
+	var cfg api.Config
+
+	err := cleanenv.ReadConfig("config.yml", &cfg)
+	if err != nil {
+		log.Fatalf("[error] config: %s\n", err)
+	}
+
+	server := api.NewServer(cfg)
 
 	_ = server.Serve()
 }
