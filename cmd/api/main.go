5186c0df803ed083d376faa18580d13542bf8954
James Liu <james@jamesliu.io>
2022-05-17 17:35:52 +1000

    feat: configure NewRelic middleware for Gin

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"log"
+
 	"omshub/core-api/internal/api"
 
 	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/newrelic/go-agent/v3/newrelic"
 )
 
 func main() {
-
 	var cfg api.Config
 
 	err := cleanenv.ReadConfig("config.yml", &cfg)
@@ -16,7 +17,26 @@ func main() {
 		log.Fatalf("[error] config: %s\n", err)
 	}
 
-	server := api.NewServer(cfg)
+	serverDeps := api.Dependencies{}
+
+	if cfg.NewRelicAPIKey == "" || cfg.NewRelicAppName == "" {
+		log.Println("[info] NewRelic configuration not provided. Continuing in the dark...")
+	}
+	if app, err := newRelicApplication(cfg.NewRelicAppName, cfg.NewRelicAPIKey); err != nil {
+		log.Printf("[warn] NewRelic could not be configured: %s\n", err)
+	} else {
+		serverDeps.NewRelicApp = app
+	}
+
+	server := api.NewServer(cfg, serverDeps)
 
 	_ = server.Serve()
+}
+
+func newRelicApplication(appName string, apiKey string) (*newrelic.Application, error) {
+	return newrelic.NewApplication(
+		newrelic.ConfigAppName(appName),
+		newrelic.ConfigLicense(apiKey),
+		newrelic.ConfigDistributedTracerEnabled(true),
+	)
 }
--- internal/api/config.go
+++ internal/api/config.go
@@ -1,5 +1,7 @@
 package api
 
 type Config struct {
-	Port string `yaml:"port" env:"SERVER_PORT" env-description:"Server port" env-default:"1927"`
+	Port            string `yaml:"port" env:"SERVER_PORT" env-description:"Server port" env-default:"1927"`
+	NewRelicAPIKey  string `env:"NEWRELIC_API_KEY" env-description:"API key for NewRelic"`
+	NewRelicAppName string `yaml:"newrelic_app_name" env:"NEWRELIC_APP_NAME" env-description:"App name for NewRelic"`
 }
