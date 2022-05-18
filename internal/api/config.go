bdf6729befa106dffd9df78838ddcaa15d82f931
ashiquem <ashiquemahmood@outlook.com>
2022-05-18 19:34:23 +1000

    feat: added env config for port

--- internal/api/config.go
+++ internal/api/config.go
@@ -1,5 +1,5 @@
 package api
 
 type Config struct {
-	Port string `yaml:"port" env-description:"Server port" env-default:"1927"`
+	Port string `yaml:"port" env:"SERVER_PORT" env-description:"Server port" env-default:"1927"`
 }
